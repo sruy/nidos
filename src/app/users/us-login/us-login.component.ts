cfcc32009291b9c5535dff8eb34f19df2921bc0d
Mathias Rodriguez <mathias.rodriguez@gmail.com>
2019-04-27 09:33:31 -0300

    feat: login action & redirect

--- src/app/users/us-login/us-login.component.ts
+++ src/app/users/us-login/us-login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UsersService } from '../users.service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-us-login',
@@ -11,7 +14,8 @@ export class UsLoginComponent implements OnInit {
   userName: string;
   password: string;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private usersService: UsersService,
+    private messageService: MessageService, private router: Router) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -23,9 +27,20 @@ export class UsLoginComponent implements OnInit {
   clearForm() {
     this.form.reset();
   }
-  
-  login(event) {
-    console.log(event, this.form.value);
-  }
 
+  login(event) {
+    if (this.form.valid) {
+      this.usersService.login(this.form.value, this.messageService).subscribe((result) => {
+        if (result && (<any>result).err) {
+          const err = (<any>result).err;
+          
+          if (err && (err.errorId === 1  || err.errorId === 2)) {
+            this.messageService.add({ severity: 'error', summary: 'No se pudo completar la operación', detail: 'Autenticación fallida' })
+          }
+        } else {
+          this.router.navigate(['/home']);
+        }
+      })
+    }
+  }
 }
