03a2944ae90d5827398f8336914392384768798f
Mathias Rodriguez <mathias.rodriguez@gmail.com>
2019-04-27 09:35:06 -0300

    feat: reflect logged status in top bar

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsersService } from '../users/users.service';
 
 @Component({
   selector: 'app-top-header',
@@ -8,15 +9,30 @@ import { Router } from '@angular/router';
 })
 export class TopHeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private usersService: UsersService) { }
+
+  get fullUserName() {
+    return this.usersService.isLogged() && `${UsersService.authUser.userName }` || ''//`${UsersService.authUser.firstName} ${UsersService.authUser.lastName}` || '';
+  }
+
+  get logged() {
+    return this.usersService.isLogged();
+  }
 
   ngOnInit() {
+    if (this.usersService.isLogged()) {
+      this.router.navigate(['/home']);
+    }
   }
 
   goToPublic() {
     this.router.navigate(['/']);
   }
 
-  signOut() { }
+  signOut() {
+    this.usersService.logOut().subscribe(() => {
+
+    });
+  }
 
 }
