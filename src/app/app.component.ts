405670ee47240961ccc38fda9cf21cef25e22496
Mathias Rodriguez <mathias.rodriguez@gmail.com>
2019-04-30 16:32:29 -0300

    feat: v0.5-beta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { MenuItem } from 'primeng/api';
 import { Gtag } from 'angular-gtag';
 import { UsersService } from './users/users.service';
 import { Router } from '@angular/router';
+import * as tz from 'moment-timezone';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +14,32 @@ import { Router } from '@angular/router';
 export class AppComponent implements OnInit {
   logged: boolean;
   menuItems: MenuItem[];
+  releaseDate = tz(moment('2019-04-30 17:00:00'), 'America/Montevideo');
+  releaseInterval: any;
+  timeRemaining = [];
+  shownTimer: string = '';
 
-  constructor(gtag: Gtag, private usersService: UsersService, private router: Router) {}
-  
+  constructor(gtag: Gtag, private usersService: UsersService, private router: Router) { }
+
   ngOnInit() {
     this.usersService.getAuthenticatedUser().subscribe((result) => {
       UsersService.authUser = result;
       this.router.navigate(['/home']);
     });
+
+    if (this.releaseDate && moment().isBefore(this.releaseDate)) {
+      
+      this.releaseInterval = window.setInterval(() => {
+        const duration = moment.duration(moment(this.releaseDate).diff(moment()));
+        this.shownTimer = '';
+        if (moment(tz(moment(), 'America/Montevideo')).isSameOrAfter(this.releaseDate)) {
+          window.clearInterval(this.releaseInterval);
+          this.releaseDate = null;
+        } else {
+          this.shownTimer = ''+duration.asMilliseconds();
+        }
+      }, 1000);
+    }
 
     this.menuItems = [
       {
